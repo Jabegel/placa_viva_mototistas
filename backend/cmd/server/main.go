8f8d4084227433be93cae5d9a176d6f50294a7fd
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2026-03-31 21:41:59 -0300

    dashboard

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -11,6 +11,9 @@ func main() {
 	// Rota de Login
 	r.POST("/auth/login", api.Login)
 
+	r.GET("/dashboard", api.GetDashboard)
+
 	// Rodar na porta 8080 em todas as interfaces de rede
 	r.Run("0.0.0.0:8080")
 }
+
